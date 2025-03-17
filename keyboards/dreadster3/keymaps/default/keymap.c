ba1c1183ec900370145c81e95c6f67fbcd980c01
dreadster3 <afonso.antunes@live.com.pt>
2025-03-17 13:36:31 +0000

    fix: swap curly braces

--- keyboards/dreadster3/keymaps/default/keymap.c
+++ keyboards/dreadster3/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,         KC_W,         KC_E,  KC_R,  KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,           KC_P,            KC_BSLS,
     LGUI_T(KC_ESC), KC_A,         KC_S,         KC_D,  KC_F,  KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,           LT(2, KC_SCLN),  RGUI_T(KC_QUOT),
     KC_LSFT,        LCTL_T(KC_Z), LALT_T(KC_X), KC_C,  KC_V,  KC_B,   KC_MUTE,      KC_MPLY, KC_N,    KC_M,    KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSFT,
-                                  KC_GRV,       KC_NO, MO(1), KC_SPC, KC_BSPC,      KC_TAB,  KC_ENT,   MO(2), KC_RBRC, KC_LBRC
+                                  KC_GRV,       KC_NO, MO(1), KC_SPC, KC_BSPC,      KC_TAB,  KC_ENT,   MO(2), KC_LBRC, KC_RBRC
   ),
   [1] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
